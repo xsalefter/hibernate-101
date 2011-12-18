0a6c98d326db8d2c833570ac20d664b7bfbe65ed
xsalefter <xsalefter@gmail.com>
2011-12-18 13:41:09 +0700

    Add method scanEntityClasses() and change some implementation in getEntityManagerFactory() and getHibernateSessionFactory() to scan an available entity in the package.

--- src/main/java/org/xsalefter/hibernate101/PersistenceUtil.java
+++ src/main/java/org/xsalefter/hibernate101/PersistenceUtil.java
@@ -1,10 +1,22 @@
 package org.xsalefter.hibernate101;
 
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.MappedSuperclass;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
 public class PersistenceUtil {
 
@@ -14,12 +26,64 @@ public class PersistenceUtil {
 	 * 
 	 * @return {@link SessionFactory} object.
 	 */
-	@SuppressWarnings("deprecation")
 	public static SessionFactory getHibernateSessionfactory() {
-		return new Configuration().configure().buildSessionFactory();
+		Configuration configuration = new Configuration();
+		
+		for (Class<?> clazz : scanEntityClasses()) {
+			configuration.addAnnotatedClass(clazz);
+		}
+		
+		configuration.addResource("hibernate.cfg.xml");
+		
+		@SuppressWarnings("deprecation")
+		final SessionFactory factory = configuration.
+			configure().
+			buildSessionFactory();
+		
+		return factory;
 	}
 	
+	@SuppressWarnings("deprecation")
 	public static EntityManagerFactory getJPAEntityManagerFactory() {
-		return Persistence.createEntityManagerFactory("example");
+		org.hibernate.ejb.Ejb3Configuration configuration = 
+			new org.hibernate.ejb.Ejb3Configuration();
+		
+		for (Class<?> clazz : scanEntityClasses()) {
+			configuration.addAnnotatedClass(clazz);
+		}
+		
+		final EntityManagerFactory factory = configuration.
+				configure("hibernate.cfg.xml").
+				buildEntityManagerFactory();
+		
+		return factory;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Set<Class<?>> scanEntityClasses() {
+		Set<Class<?>> result = new HashSet<Class<?>>();
+		
+		Set<URL> packages = ClasspathHelper.forPackage("org.xsalefter.hibernate101.entity");
+		
+		Set<URL> entityAnnotation = new HashSet<URL>();
+		entityAnnotation.add(ClasspathHelper.forClass(Entity.class));
+		entityAnnotation.add(ClasspathHelper.forClass(MappedSuperclass.class));
+		entityAnnotation.add(ClasspathHelper.forClass(Embeddable.class));
+		
+		ConfigurationBuilder builder = new ConfigurationBuilder().
+				addUrls(packages, entityAnnotation)
+				.setScanners(
+					new ResourcesScanner(), 
+					new TypeAnnotationsScanner(),
+					new SubTypesScanner()
+				);
+		
+		Reflections reflections = new Reflections(builder);
+		
+		result.addAll(reflections.getTypesAnnotatedWith(Entity.class));
+		result.addAll(reflections.getTypesAnnotatedWith(MappedSuperclass.class));
+		result.addAll(reflections.getTypesAnnotatedWith(Embeddable.class));
+		
+		return result;
 	}
 }
