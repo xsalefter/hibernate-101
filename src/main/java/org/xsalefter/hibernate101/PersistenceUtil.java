766276821629ee4dd69041e7d0a09521a175a5a7
xsalefter <xsalefter@gmail.com>
2011-12-18 08:12:51 +0700

    Add comment and @SuppressWarnings("deprecation") to getHibernateSessionfactory() method.

--- src/main/java/org/xsalefter/hibernate101/PersistenceUtil.java
+++ src/main/java/org/xsalefter/hibernate101/PersistenceUtil.java
@@ -8,6 +8,13 @@ import org.hibernate.cfg.Configuration;
 
 public class PersistenceUtil {
 
+	/**
+	 * Since Hibernate 4.0 {@link Configuration#configure#buildSessionFactory()} 
+	 * is deprecated. 
+	 * 
+	 * @return {@link SessionFactory} object.
+	 */
+	@SuppressWarnings("deprecation")
 	public static SessionFactory getHibernateSessionfactory() {
 		return new Configuration().configure().buildSessionFactory();
 	}
