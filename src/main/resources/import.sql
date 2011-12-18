d29c87b18829d2f87b65b7404ea73719f52cdcdc
xsalefter <xsalefter@gmail.com>
2011-12-18 08:15:20 +0700

    Delete all files in main/resources/** . We have one in test/resources/**

--- src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-insert into student (student_id, name, email) values (1, 'student 1', 'student1@college.com');
-insert into student (student_id, name, email) values (2, 'student 2', 'student2@college.com');
-insert into student (student_id, name, email) values (3, 'student 3', 'student3@college.com');
-insert into student (student_id, name, email) values (4, 'student 4', 'student4@college.com');
-insert into student (student_id, name, email) values (5, 'student 5', 'student5@college.com');
