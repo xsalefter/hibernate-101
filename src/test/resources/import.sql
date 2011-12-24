43503e0ef0c049246624fb3aa4489aef7745cc93
xsalefter <xsalefter@gmail.com>
2011-12-24 15:49:02 +0700

    Add Product.java and ProductCategory.java

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -3,3 +3,17 @@ insert into student (student_id, name, email) values (2, 'student 2', 'student2@
 insert into student (student_id, name, email) values (3, 'student 3', 'student3@college.com');
 insert into student (student_id, name, email) values (4, 'student 4', 'student4@college.com');
 insert into student (student_id, name, email) values (5, 'student 5', 'student5@college.com');
+
+insert into product_category (category_id, category_name) values (1, 'Book');
+insert into product_category (category_id, category_name) values (2, 'Home Electronic');
+insert into product_category (category_id, category_name) values (3, 'Notebook');
+insert into product_category (category_id, category_name) values (4, 'Smart Phone');
+
+insert into product (product_id, product_name, default_price, category_id) values (1, 'Head First Design Pattern', '43.00', 1);
+insert into product (product_id, product_name, default_price, category_id) values (2, 'Design Pattern - Elements of Reusable Object Oriented Software', '51.00', 1);
+insert into product (product_id, product_name, default_price, category_id) values (3, 'Domain-Driven Design: Tackling Complexity in the Heart of Software', '45.00', 1);
+insert into product (product_id, product_name, default_price, category_id) values (4, 'Effective Java, 2nd Edition', '42.00', 1);
+insert into product (product_id, product_name, default_price, category_id) values (5, 'Getting Things Done: The Art of Stress-Free Productivity', '29.00', 1);
+insert into product (product_id, product_name, default_price, category_id) values (6, 'Samsung LCD TV 32 inch', '229.00', 2);
+insert into product (product_id, product_name, default_price, category_id) values (7, 'Toshiba Bravia LED TV 32 inch', '395.00', 2);
+insert into product (product_id, product_name, default_price, category_id) values (8, 'Sharp LED TV 40 inch', '519.00', 2);
